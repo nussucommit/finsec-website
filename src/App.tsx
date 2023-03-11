f231892cf66a04967fa93b12f3e2b07e474faaab
tryyang2001 <e0550472@u.nus.edu>
2023-03-11 17:56:03 +0800

    resolve merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./table/table.css";
+import { ResourceTable } from './table/ResourceTable';
+import { ActionableResourceTable } from './table/ActionableResourceTable';
+import { RfpTable } from './table/RfpTable';
+import { PendingRfpTable } from './table/PendingRfpTable';
 
 function App() {
+  var resourceTable = new ResourceTable();
+  resourceTable.addTableContent("Test", "http://test.com");
+
+  var actionableResourceTable = new ActionableResourceTable();
+  actionableResourceTable.addTableContent("Test Action", "http://test.com");
+
+  var rfpTable = new RfpTable();
+  rfpTable.addTableContent("RFP1008024", "11/3/2023", "rejected", "Tan Rui Yang");
+
+  var pendingRfpTable = new PendingRfpTable();
+  pendingRfpTable.addTableContent(
+    "RFP1008024",
+    "11/3/2023",
+    "rejected",
+    "Tan Rui Yang"
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {resourceTable.renderTable()}
+      {actionableResourceTable.renderTable()}
+      {rfpTable.renderTable()}
+      {pendingRfpTable.renderTable()}
     </div>
   );
 }
